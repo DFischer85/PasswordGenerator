baa7928534412223c7dac628192700be33629791
Daniel Fischer <DFischer85@comcast.net>
2021-08-31 05:08:00 -0400

    Password Generator Complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'passwordgenerator';
+
+  password = '';
+  length = 0;
+  useLetters = false;
+  useNumbers = false;
+  useSymbols = false;
+
+  onButtonClick(){
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+    if(this.useLetters)
+      validChars += letters;
+    
+    if(this.useNumbers)
+      validChars += numbers;
+
+    if(this.useSymbols)
+      validChars += symbols;
+
+    let generatedPassword = '';
+
+    for (let i = 0; i < this.length; i++){
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    
+    this.password = generatedPassword;
+
+  }
+
+  onChangeUseLetters(){
+    this.useLetters = !this.useLetters;
+  }
+
+  onChangeUseNumbers(){
+    this.useNumbers = !this.useNumbers;
+  }
+
+  onChangeUseSymbols(){
+    this.useSymbols = !this.useSymbols;
+  }
+
+  onChangeLength(value: string){
+    const parsedValue = parseInt(value);
+    if(!isNaN(parsedValue)){
+      this.length = parsedValue;
+    }
+  }
+
 }
